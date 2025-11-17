8369bb6227e1e24045ab923a960791c68e7eea38
zhumagulovv <jakypjumagulov080@gmail.com>
2025-11-17 17:56:02 +0600

    number

---
+++ dataTypes.py
@@ -0,0 +1,36 @@
+x = 20
+name = "Zhakyp"
+numbers = [1,2,3,4,5]
+a = 20.4
+fruits = ("Cherry", "Apple", "Mango")
+user = {"name": "Zhakyp", "age": 20}
+users = {"Asan", "Uson", "Almaz"}
+isMarried = False
+sum = range(4)
+
+# int
+print(type(x))
+
+# str
+print(type(name))
+
+# list
+print(type(numbers))
+
+# float
+print(type(a))
+
+# tuple
+print(type(fruits))
+
+# dict
+print(type(user))
+
+# set
+print(type(users))
+
+# bool
+print(type(isMarried))
+
+# range
+print(type(sum))
