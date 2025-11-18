fb31ff89f1310a66d05d0889c25496f836317da4
zhumagulovv <jakypjumagulov080@gmail.com>
2025-11-18 17:41:04 +0600

    casting

---
+++ casting.py
@@ -0,0 +1,30 @@
+# int()
+# float()
+# str()
+
+# int()
+a = int(2.5)
+b = int("3")
+c = int(5)
+
+print("int():", a, type(a))
+print("int():", b, type(b))
+print("int():", c, type(c))
+
+# float()
+d = float(3)
+f = float(2.4)
+j = float("4")
+
+print("float():", d, type(d))
+print("float():", f, type(f))
+print("float():", j, type(j))
+
+# str()
+x = str(10)
+y = str(10.5)
+z = str("20")
+
+print("str():", x, type(x))
+print("str():", y, type(y))
+print("str():", z, type(z))
