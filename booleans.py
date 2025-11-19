b487f56d0cfa5e4fa540073fcecb65ee178a712f
zhumagulovv <jakypjumagulov080@gmail.com>
2025-11-19 17:45:34 +0600

    boolean

---
+++ booleans.py
@@ -0,0 +1,21 @@
+print(10 > 9)
+print(10 < 9)
+print(10 == 10)
+print(10 == 9)
+
+a = 10
+b = 9
+c = "Hello"
+
+if a < b:
+    print(f"{a} больше")
+else:
+    print(f"{a} меньше")
+
+print(bool(a))
+print(bool("Hello"))
+print(bool(0))
+print(bool([]))
+
+print(isinstance(a, int))
+print(isinstance(c, int))
