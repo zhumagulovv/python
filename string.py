8249ba806e57c541ae013a3bbc244ee84ebe87b4
zhumagulovv <jakypjumagulov080@gmail.com>
2025-11-19 17:27:12 +0600

    string

---
+++ string.py
@@ -0,0 +1,29 @@
+print("Hello")
+print('Hello!')
+
+print("It's a alright")
+print("She's a very beautiful")
+
+message = "Welcome to Bishkek!"
+
+print(message)
+
+# array []
+print(message[11])
+
+# length
+print(len(message))
+
+# check
+print("to" in message)
+
+for i in "banana":
+    print(i)
+
+if "to" in message:
+    print("Kyrgyzstan")
+
+print("Astana" not in message)
+
+if "Astana" not in message:
+    print("Welcome to Astana!")
