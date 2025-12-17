55eaa89a19a85b8addb7c0d9ab8df868b8735164
zhumagulovv <jakypjumagulov080@gmail.com>
2025-12-17 17:44:13 +0600

    Arrays

---
+++ arrays.py
@@ -0,0 +1,23 @@
+cars = ["Audi", "BMW", "Volvo", "Lexus"]
+
+cars.append("Mercedes")
+cars.append("Honda")
+
+# cars.pop()
+# cars.pop()
+
+cars.remove("BMW")
+
+# cars.clear()
+cars.sort()
+cars.reverse()
+
+print(cars)
+print(cars[0])
+print(cars[1])
+print(cars[2])
+print(len(cars))
+
+for car in cars:
+    print(car)
+
