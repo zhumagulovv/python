59de88bb2c62ff63220a7959a479303a95d41e16
zhumagulovv <jakypjumagulov080@gmail.com>
2025-12-24 17:29:37 +0600

    math

---
+++ math.py
@@ -0,0 +1,23 @@
+import math
+
+a = max(1, 2, 3, 4)
+b = min(1, 2, 3, 4)
+c = a + b
+d = abs(-13.95)
+j = pow(30, 2)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(j)
+
+i = math.sqrt(64)
+x = math.ceil(1.4)
+y = math.floor(1.4)
+z = math.pi
+
+print(i)
+print(x)
+print(y)
+print(z)
